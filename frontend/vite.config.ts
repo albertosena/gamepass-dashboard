00e668e003689e9971ccb05cc0c839efbe3ad916
Alberto <albertosena6@gmail.com>
2025-12-01 21:53:50 -0300

    Remove example Docker Compose file and update Vite configuration for server settings

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: true,
+    host: true,    // importante em Docker / túnel
+    port: 5173,
+  },
 })
